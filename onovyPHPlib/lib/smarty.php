7525950dc2412cf0f813d61a05c38850723748b3
Ondřej Nový <novy@ondrej.org>
2026-03-16 12:27:40 +0100

    Upgrade Smarty from 2.6.31 to 5.8.0
    Replace the vendored Smarty 2.x tree with Smarty 5.8.0 source,
    installed without Composer using the libs/Smarty.class.php PSR-4
    stub loader.
    
    Update onovyPHPlib/lib/smarty.php for the Smarty 5 API:
    - require_once now points to libs/Smarty.class.php
    - add use Smarty\Smarty namespace import
    - replace $smarty->compile_check = ... with setCompileCheck()
    - add explicit absolute-path setters for template, compile,
      cache and config directories using WEB_DIR
    
    No template changes required: {section}/$smarty.section.*,
    {config_load}/{#key#} and all other syntax in use is fully
    supported in Smarty 5.

--- onovyPHPlib/lib/smarty.php
+++ onovyPHPlib/lib/smarty.php
@@ -1,9 +1,16 @@
 <?php
-require_once OLIB_DIR . '/extlib/Smarty/Smarty.class.php';
+require_once OLIB_DIR . '/extlib/Smarty/libs/Smarty.class.php';
+
+use Smarty\Smarty;
 
 $smarty = new Smarty;
 
-$smarty->compile_check = $local_config['compile_check'];
+$smarty->setTemplateDir(WEB_DIR . '/templates');
+$smarty->setCompileDir(WEB_DIR . '/templates_c');
+$smarty->setCacheDir(WEB_DIR . '/cache');
+$smarty->setConfigDir(WEB_DIR . '/configs');
+
+$smarty->setCompileCheck($local_config['compile_check']);
 
 function sql2smarty($s,$array) {
  $a=0;
