899c98c708efe0fb759dd7119aa1eeee30da2cc7
Ondřej Nový <novy@ondrej.org>
2026-03-16 13:44:23 +0100

    Fix double bracets

--- graph/graph.php
+++ graph/graph.php
@@ -5,7 +5,7 @@ require_once 'onovyPHPlib/lib/sql.php';
 
 $count=db_fquery('SELECT COUNT(*) FROM online');
 $count=(int)$count[0];
-system('rrdupdate rrd/online.rrd ' . escapeshellarg(time() . ':"'  . $count . '"'));
+system('rrdupdate rrd/online.rrd ' . escapeshellarg(time() . ':'  . $count));
 
 
 system('rrdtool graph rrd/online.png -W xkviz.net --title "Online" --start now-1d --units-exponent 0 --rigid --imgformat PNG --width 576 --height 200 --base 1000 --lower-limit 0 --vertical-label Count '.
